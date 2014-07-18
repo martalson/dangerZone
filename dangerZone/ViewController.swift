11c133eebc188d9efa5e336f24d930858bd61778
martalson <martinalonso@berkeley.edu>
2014-07-17 19:31:05 -0700

    Initial Commit

---
+++ dangerZone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dangerZone
+//
+//  Created by Martin Alonso on 7/17/14.
+//  Copyright (c) 2014 martalson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

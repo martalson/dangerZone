4e1f7eb58cb90bcf42c4d387ed0f6d5b67e25b0b
martalson <martinalonso@berkeley.edu>
2014-07-18 16:47:30 -0700

    Facebook api added

--- dangerZone/ViewController.swift
+++ dangerZone/ViewController.swift
@@ -13,16 +13,25 @@ class ViewController: UIViewController, GMSMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var gmps: GMSMapView = GMSMapView(frame: CGRectZero)
-        var targ = CLLocationCoordinate2D(latitude: 42.4, longitude: 1.68)
-        var camera: GMSCameraPosition = GMSCameraPosition(target: targ, zoom: 6, bearing: 0, viewingAngle: 0)
-        gmps.myLocationEnabled = true
-        self.view = gmps
-        var maaaHouse = CLLocationCoordinate2DMake(42.06, 1.6)
-        var mark = GMSMarker(position: maaaHouse)
-        mark.title = "maaaHouse"
-        mark.snippet = "Tudela"
-        mark.map = gmps
+//        var gmps: GMSMapView = GMSMapView(frame: CGRectZero)
+//        var targ = CLLocationCoordinate2D(latitude: 42.4, longitude: 1.68)
+//        var camera: GMSCameraPosition = GMSCameraPosition(target: targ, zoom: 6, bearing: 0, viewingAngle: 0)
+//        gmps.myLocationEnabled = true
+//        self.view = gmps
+//        var maaaHouse = CLLocationCoordinate2DMake(42.06, 1.6)
+//        var mark = GMSMarker(position: maaaHouse)
+//        mark.title = "maaaHouse"
+//        mark.snippet = "Tudela"
+//        mark.map = gmps
+        //access my location
+        //display my location
+        //send my location to server every x seconds
+        //set up notif listener, if it receives trigger, new view with match info
+        
+        //-- in server -- 
+        //change the data associated with the user
+        //check whether there is a match in server
+        //if there is send notif
         
     }
 

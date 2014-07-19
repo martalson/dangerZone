ca8f80fb641e1a9889e49fb65ec51026ae44b435
martalson <martinalonso@berkeley.edu>
2014-07-18 19:37:48 -0700

    added a post request

--- dangerZone/ViewController.swift
+++ dangerZone/ViewController.swift
@@ -12,17 +12,25 @@ class ViewController: UIViewController, GMSMapViewDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        var gmps: GMSMapView = GMSMapView(frame: CGRectZero)
-//        var targ = CLLocationCoordinate2D(latitude: 42.4, longitude: 1.68)
-//        var camera: GMSCameraPosition = GMSCameraPosition(target: targ, zoom: 6, bearing: 0, viewingAngle: 0)
-//        gmps.myLocationEnabled = true
-//        self.view = gmps
-//        var maaaHouse = CLLocationCoordinate2DMake(42.06, 1.6)
-//        var mark = GMSMarker(position: maaaHouse)
-//        mark.title = "maaaHouse"
-//        mark.snippet = "Tudela"
-//        mark.map = gmps
+                var gmps: GMSMapView = GMSMapView(frame: CGRectZero)
+                var targ = CLLocationCoordinate2D(latitude: 37.87, longitude: 238.73)
+                var camera: GMSCameraPosition = GMSCameraPosition(target: targ, zoom: 6, bearing: 0, viewingAngle: 0)
+                gmps.myLocationEnabled = true
+                self.view = gmps
+                gmps.camera = camera
+                var mark = GMSMarker(position: targ)
+                mark.title = "Me"
+                mark.snippet = "Hack"
+                mark.map = gmps
+        
+                var url = NSURL(string: "http://cole")
+                var request = NSMutableURLRequest(URL: url)//initialize request from url
+                request.HTTPMethod = "POST" //set http method
+                var postData: NSData = NSKeyedArchiver.archivedDataWithRootObject([789, 37.87, 238.73])//transform array into data
+                request.HTTPBody = postData //put data in body
+                var connection = NSURLConnection(request: request, delegate: self) //initialize connection
+                connection.start()
+        
         //access my location
         //display my location
         //send my location to server every x seconds
